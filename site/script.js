460966f0ada78034767c3661d1040fe6ba61e386
Enio Júnior <eniolord12@gmail.com>
2026-03-26 18:37:11 -0300

    Json adcionado

--- site/script.js
+++ site/script.js
@@ -1,13 +1,19 @@
-const carouselImages = [
-    { src: 'img/ImgDeDestaques/Bancada-enio.jpg', alt: 'Destaque DEWALT' },
-    { src: 'img/ImgDeDestaques/bancada-eniojr.jpg', alt: 'Destaque STANLEY' },
-    { src: 'img/ImgDeDestaques/fachada.jpg', alt: 'Destaque BLACK+DECKER' },
-];
-
+let carouselImages = [];
 const carouselContainer = document.querySelector('.carousel-container');
 const dotContainer = document.querySelector('.dots');
 let currentIndex = 0;
 let intervalId;
+
+async function loadImages() {
+    try {
+        const response = await fetch('img/ImgDeDestaques/images.json');
+        carouselImages = await response.json();
+        setupDots();
+        startCarousel();
+    } catch (error) {
+        console.error('Erro ao carregar imagens:', error);
+    }
+}
 
 function updateCarousel(index) {
     const imageData = carouselImages[index];
@@ -33,8 +39,10 @@ function nextImage() {
 }
 
 function startCarousel() {
-    updateCarousel(currentIndex);
-    intervalId = setInterval(nextImage, 2500);
+    if (carouselImages.length > 0) {
+        updateCarousel(currentIndex);
+        intervalId = setInterval(nextImage, 2500);
+    }
 }
 
 function setupDots() {
@@ -56,6 +64,5 @@ function setupDots() {
 }
 
 if (carouselContainer && dotContainer) {
-    setupDots();
-    startCarousel();
+    loadImages();
 }
